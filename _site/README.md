f315b1e68838b25fde275a94136e199c7819bd18
Attila Molnar <attilamolnar@hush.com>
2016-02-02 12:12:20 +0100

    rm -rf _site

--- _site/README.md
+++
@@ -1,3 +0,0 @@
-### All new website for inspircd
-
-> Please before deploy ***do not forget*** to generate css from ```_scr > _scss > inspircd.scss```
--- _site/assets/scripts/inspircd.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    console.log('Welcome to InspIRCD!');
-    console.log('Join us on IRC | irc.chatspike.net/6667 | #inspircd');
-});
